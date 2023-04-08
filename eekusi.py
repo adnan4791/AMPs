dac2bdaef7ca4b0e50a6ce246d08dc029e7ab6a3
adnan4791 <110278005+adnan4791@users.noreply.github.com>
2023-04-09 07:42:33 +0800

    Delete eekusi.py

--- eekusi.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-from itertools import product
-
-for x,y in product(['8','4','2','1'],range(10)) :
-    filename = "serial_performance_core"
-    with open(filename,'a') as f:
-        subprocess.run(["./pi_on_performance",x],stdout=f,universal_newline=true)
